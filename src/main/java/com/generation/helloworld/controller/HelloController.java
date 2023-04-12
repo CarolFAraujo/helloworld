a0e2373ebf11eb6ee624ba339b5f4176f1a6ebb0
CarolFAraujo <carolina.fac@gmail.com>
2023-04-12 15:21:04 -0300

    Atividade Hello World

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -4,19 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/hello") //anotação deve ficar junta da classe, sem dar enter para pular linha
+@RequestMapping("/hello") // anotação deve ficar junta da classe, sem dar enter para pular linha
 public class HelloController {
-	
+
 	@GetMapping
 	public String hello() {
-		return "<b>Hello Turma 62!</b>";
-	}
-	
-	@GetMapping("/nome")
-	public String meunome() {
-		return "<b> Hello! <br />Carolina</b>";
+		return "<b>Hello World!</b>";
 	}
 
+	@GetMapping("/bsm")
+	public String bsms() {
+	    return "<b>Mentalidade de crescimento</b><br>Persistência<br>Responsabilidade pessoal<br>Orientação ao futuro<br>Comunicação<br>Orientação ao detalhe<br>Proatividade<br>Trabalho em equipe";
+
+	}
+
+	@GetMapping("/objetivos")
+	public String objetivos() {
+	    return "<b>Objetivos de aprendizagem da semana:</b><br>Melhorar a atenção aos detalhes;<br>Aumentar a minha comunicação com o time;<br>Continuar focada no trabalho em equipe.";
+	}
 }
